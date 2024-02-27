b919d5a73728dfefdb69b9cc3799afb2017a7f98
Ayhan Krala <codewolf27@gmail.com>
2024-02-27 14:49:42 +0200

    implemented job service interface with needed methods

---
+++ src/main/java/com/jobapplication/firstjobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,18 @@
+package com.jobapplication.firstjobapp.job.impl;
+
+import com.jobapplication.firstjobapp.job.Job;
+import com.jobapplication.firstjobapp.job.JobService;
+
+import java.util.List;
+
+public class JobServiceImpl implements JobService {
+    @Override
+    public List<Job> findAll() {
+        return null;
+    }
+
+    @Override
+    public void createJob() {
+
+    }
+}
