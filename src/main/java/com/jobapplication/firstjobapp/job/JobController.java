e3bb2f60a2026f55aadc1756411e179e7370de94
Ayhan Krala <codewolf27@gmail.com>
2024-02-27 17:11:00 +0200

    Improve to getting a single job

--- src/main/java/com/jobapplication/firstjobapp/job/JobController.java
+++ src/main/java/com/jobapplication/firstjobapp/job/JobController.java
@@ -1,30 +1,38 @@
 package com.jobapplication.firstjobapp.job;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @RestController
 public class JobController {
-    private JobService jobService ;
+    private JobService jobService;
 
     public JobController(JobService jobService) {
         this.jobService = jobService;
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
+    public List<Job> findAll() {
         return jobService.findAll();
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public String createJob(@RequestBody Job job) {
         jobService.createJob(job);
         return "Job added successfully";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public Job getJobById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        if (job != null) {
+            return job ;
+        }
+        return new Job(1L,"Test Job","Test Job","2000"
+                ,"2000","Turkey");
     }
 
 }
