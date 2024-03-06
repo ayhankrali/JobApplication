ad3063983bc7380c2b9ffad436a2ba96e89426a9
Ayhan Krala <codewolf27@gmail.com>
2024-03-06 20:53:10 +0200

    Implement the post-method for updating the reviews

--- src/main/java/com/jobapplication/firstjobapp/review/ReviewController.java
+++ src/main/java/com/jobapplication/firstjobapp/review/ReviewController.java
@@ -40,10 +40,28 @@ public class ReviewController {
     @GetMapping("/reviews/{reviewId}")
     public ResponseEntity<Review> getReview(@PathVariable Long companyId,
                                             @PathVariable Long reviewId) {
-        return new ResponseEntity<>(reviewService.getReview(companyId,reviewId),
+        return new ResponseEntity<>(reviewService.getReview(companyId, reviewId),
                 HttpStatus.OK);
     }
+
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(@PathVariable Long companyId,
+                                               @PathVariable Long reviewId,
+                                               @RequestBody Review review) {
+        boolean isReviewUpdated = reviewService.updateReview(companyId, reviewId, review);
+        if (isReviewUpdated) {
+            return new ResponseEntity<>("Review updated successfully",
+                    HttpStatus.OK);
+
+        } else {
+            return new ResponseEntity<>("Review not updated",
+                    HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
+
 
 
 
